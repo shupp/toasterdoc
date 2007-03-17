729cfe050530cedddeaf3bf4cfcce9a133e0c0f8
shupp <shupp@5df0e5c9-6029-0410-b58c-b86a8e687cad>
2007-03-17 00:47:57 +0000

    update language script and messages files (en only)
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasterdoc@48 5df0e5c9-6029-0410-b58c-b86a8e687cad

--- trunk/update_lang.sh
+++ trunk/update_lang.sh
@@ -3,13 +3,7 @@
 LCDIR='locale/en/LC_MESSAGES'
 MFILE="$LCDIR/messages.po"
 
-#TEMPFILE="/tmp/gettext_files.$$"
-
-#find ../ | egrep -v '(.svn|Templates/Default/templates_c|Templates/Default/cache)' | egrep '(.php$)' > $TEMPFILE
-
-#xgettext -L PHP --keyword=_ public/index.php Includes/*php Modules/*php tpl/* --output=$MFILE
-xgettext -L PHP --keyword=_ test.xml --output=$MFILE
+xgettext -L PHP --keyword=_ index.php tpl/* --output=$MFILE
 sed -i -e 's/CHARSET/UTF-8/' $MFILE
 sed -i -e 's!FULL NAME <EMAIL@ADDRESS>!Bill Shupp <hostmaster@shupp.org>!' $MFILE
 (cd $LCDIR ; msgfmt messages.po)
-#rm $TEMPFILE
